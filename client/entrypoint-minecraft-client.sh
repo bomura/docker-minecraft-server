eb42628495c0abf8491052eeeeddc512902479c8
kaz neko <kaz_neko@mail.donguri3.net>
2018-12-01 22:57:24 +0900

    Introduce AETHERII-1.12.2

---
+++ client/entrypoint-minecraft-client.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+mkdir -p forge mods
+curl -L -o ./forge/forge-1.12.2-14.23.5.2768-installer.jar https://files.minecraftforge.net/maven/net/minecraftforge/forge/1.12.2-14.23.5.2768/forge-1.12.2-14.23.5.2768-installer.jar
+curl -L -o ./mods/aether_ii-1.12.2-1.1.12.jar https://minecraft.curseforge.com/projects/the-aether-ii/files/2630226/download
+curl -L -o ./mods/orbis_api-1.12.2-1.1.12a.jar https://minecraft.curseforge.com/projects/orbis-api/files/2631531/download
+
+exec $*
