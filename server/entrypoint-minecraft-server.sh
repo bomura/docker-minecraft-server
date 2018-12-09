c7169655e42a9e74163fda135ab40bef8c78bba0
kaz neko <kaz_neko@mail.donguri3.net>
2018-12-09 18:05:42 +0900

    add skyblock world

--- server/entrypoint-minecraft-server.sh
+++ server/entrypoint-minecraft-server.sh
@@ -11,9 +11,17 @@ then
   ops=${ops:0:15}-${ops:15:4}-${ops:19:4}-${ops:23:4}-${ops:27}
   ops=${ops//id/uuid}
   ops="[${ops%\}},\"level\":4,\"bypassesPlayerLimit\":true}]"
-  
+
   echo ${ops} > /dev/stdout
   echo ${ops} > ops.json
+fi
+
+if [ ! -e './world/data' ]
+then
+    curl -LO 'https://minecraft.curseforge.com/projects/skyblock-3/files/2455897/download'
+    unzip download
+    mv './Skyblock 3.07'/* './world/'
+    rm -rf './Skyblock 3.07'
 fi
 
 
